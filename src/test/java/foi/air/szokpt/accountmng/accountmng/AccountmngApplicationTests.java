b2d4890ce5beaa23e94a2dfffcf0cd03622d52d4
NinoLoncar <nino.loncar16@gmail.com>
2024-11-08 12:01:25 +0100

    Inicijalni projekt
    Projekt je kreiran koristeći spring initialaizr
    Postavke projekta: Maven, Java 23, Spring boot 3.3.5, JAR

---
+++ src/test/java/foi/air/szokpt/accountmng/accountmng/AccountmngApplicationTests.java
@@ -0,0 +1,13 @@
+package foi.air.szokpt.accountmng.accountmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountmngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
