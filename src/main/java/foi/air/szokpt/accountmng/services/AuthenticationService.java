d60a653ad7485a9c63e261f8e4b341e3eb9d395a
NinoLoncar <nino.loncar16@gmail.com>
2024-11-08 20:22:09 +0100

    MI5-28 Pojednostavljena logika authenticate metode

--- src/main/java/foi/air/szokpt/accountmng/services/AuthenticationService.java
+++ src/main/java/foi/air/szokpt/accountmng/services/AuthenticationService.java
@@ -4,15 +4,13 @@ import foi.air.szokpt.accountmng.entitites.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class AuthenticationService {
     private final List<User> users = List.of(new User("admin", "admin"));
 
-    public Optional<User> authenticate(String username, String password) {
-        return users.stream().filter(user ->
-                        user.getUsername().equals(username) && user.getPassword().equals(password))
-                .findFirst();
+    public boolean authenticate(String username, String password) {
+        return users.stream().anyMatch(user ->
+                user.getUsername().equals(username) && user.getPassword().equals(password));
     }
 }
