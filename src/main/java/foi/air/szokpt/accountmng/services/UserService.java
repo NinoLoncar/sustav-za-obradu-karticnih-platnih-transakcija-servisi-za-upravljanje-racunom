5b1790dd17042d27bdcb17b52efa87e16a71534b
kmilos-foi <kmilos21@foi.hr>
2024-11-23 18:56:43 +0100

    MI5-38 Stvoren UserService

--- src/main/java/foi/air/szokpt/accountmng/services/UserService.java
+++ src/main/java/foi/air/szokpt/accountmng/services/UserService.java
@@ -2,18 +2,27 @@ package foi.air.szokpt.accountmng.services;
 
 import foi.air.szokpt.accountmng.entitites.User;
 import foi.air.szokpt.accountmng.repositories.UserRepository;
+import foi.air.szokpt.accountmng.util.validation.UserDataValidator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    UserDataValidator userDataValidator;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserDataValidator userDataValidator) {
         this.userRepository = userRepository;
+        this.userDataValidator = userDataValidator;
     }
 
+
     public void registerUser(User user) {
-        this.userRepository.save(user);
+        userDataValidator.validateRequiredFields(user);
+        userDataValidator.validateEmailFormat(user.getEmail());
+        userDataValidator.validateEmailUniqueness(user.getEmail());
+        userDataValidator.validateUsernameUniqueness(user.getUsername());
+
+        userRepository.save(user);
     }
 
 }
