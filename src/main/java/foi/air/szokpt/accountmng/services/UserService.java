f830e899b33ac018bef7672773a94b129d2e676a
kmilos-foi <kmilos21@foi.hr>
2024-12-02 16:39:44 +0100

    MI5-38 Dodano hashiranje lozinke
    Lozinka se hashira prije spremanja u bazu podataka

--- src/main/java/foi/air/szokpt/accountmng/services/UserService.java
+++ src/main/java/foi/air/szokpt/accountmng/services/UserService.java
@@ -2,17 +2,20 @@ package foi.air.szokpt.accountmng.services;
 
 import foi.air.szokpt.accountmng.entitites.User;
 import foi.air.szokpt.accountmng.repositories.UserRepository;
+import foi.air.szokpt.accountmng.util.hashing.Hasher;
 import foi.air.szokpt.accountmng.util.validation.Validator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    Validator<User> userDataValidator;
+    private final Validator<User> userDataValidator;
+    private final Hasher hasher;
 
-    public UserService(UserRepository userRepository, Validator<User> validator) {
+    public UserService(UserRepository userRepository, Validator<User> validator, Hasher hasher) {
         this.userRepository = userRepository;
         this.userDataValidator = validator;
+        this.hasher = hasher;
     }
 
     public void registerUser(User user) {
@@ -23,6 +26,8 @@ public class UserService {
         if (userRepository.existsByUsername(user.getUsername())) {
             throw new RuntimeException("User with this username already exists");
         }
+
+        user.setPassword(hasher.hashText(user.getPassword()));
         userRepository.save(user);
     }
 
