cbe0f7f1a6ad9cab7670d5cb1279f086e490d849
kmilos-foi <kmilos21@foi.hr>
2024-12-02 15:46:04 +0100

    MI5-38 Dodana provjera jwt-a u UserControlleru
    UserController koristi JwtService koji je zadužen za verifikaciju dobivenog tokena

--- src/main/java/foi/air/szokpt/accountmng/controllers/UserController.java
+++ src/main/java/foi/air/szokpt/accountmng/controllers/UserController.java
@@ -2,29 +2,39 @@ package foi.air.szokpt.accountmng.controllers;
 
 import foi.air.szokpt.accountmng.dtos.respones.ApiResponse;
 import foi.air.szokpt.accountmng.entitites.User;
+import foi.air.szokpt.accountmng.services.JwtService;
 import foi.air.szokpt.accountmng.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     private final UserService userService;
+    private final JwtService jwtService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService) {
         this.userService = userService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse> register(@RequestBody User user) {
+    public ResponseEntity<ApiResponse> register(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestBody User user) {
         try {
+            String token = authorizationHeader.substring(7);
+            Boolean isTokenValid = jwtService.verifyToken(token);
+            if (!isTokenValid) return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse("Token is not valid"));
             userService.registerUser(user);
             return ResponseEntity.status(HttpStatus.CREATED)
-                    .body(new ApiResponse("User successfully registered."));
+                    .body(new ApiResponse("User successfully registered"));
         }
         catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
