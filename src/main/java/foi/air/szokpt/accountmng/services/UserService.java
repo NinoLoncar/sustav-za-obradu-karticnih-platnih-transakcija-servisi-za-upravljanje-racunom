c823c12b64cecb9010385d98e3409f0c54baa3dd
kmilos-foi <kmilos21@foi.hr>
2024-12-02 20:01:09 +0100

    MI5-38 Dodana metoda dodjelu uloge
    Postavljanje cijelog objekta UserRole na temelju naziva UserRole-a.

--- src/main/java/foi/air/szokpt/accountmng/services/UserService.java
+++ src/main/java/foi/air/szokpt/accountmng/services/UserService.java
@@ -1,34 +1,43 @@
 package foi.air.szokpt.accountmng.services;
 
 import foi.air.szokpt.accountmng.entitites.User;
+import foi.air.szokpt.accountmng.entitites.UserRole;
+import foi.air.szokpt.accountmng.exceptions.ValidationException;
+import foi.air.szokpt.accountmng.repositories.RoleRepository;
 import foi.air.szokpt.accountmng.repositories.UserRepository;
 import foi.air.szokpt.accountmng.util.hashing.Hasher;
 import foi.air.szokpt.accountmng.util.validation.Validator;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final Validator<User> userDataValidator;
     private final Hasher hasher;
 
-    public UserService(UserRepository userRepository, Validator<User> validator, Hasher hasher) {
+    public UserService(UserRepository userRepository, RoleRepository roleRepository, Validator<User> validator, Hasher hasher) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.userDataValidator = validator;
         this.hasher = hasher;
     }
 
     public void registerUser(User user) {
+        user.setEmail(user.getEmail().toLowerCase());
         userDataValidator.validateData(user);
-        if (userRepository.existsByEmail(user.getEmail())) {
-            throw new RuntimeException("User with this email already exists");
-        }
-        if (userRepository.existsByUsername(user.getUsername())) {
-            throw new RuntimeException("User with this username already exists");
-        }
-
+        assignRoleToUser(user);
         user.setPassword(hasher.hashText(user.getPassword()));
         userRepository.save(user);
+    }
+
+    private void assignRoleToUser(User user) {
+        Optional<UserRole> userRole = roleRepository.findByName(user.getRole().getName());
+        UserRole role = userRole.orElseThrow(() ->
+                new ValidationException("Role with name '" + user.getRole().getName() + "' does not exist"));
+        user.setRole(role);
     }
 
 }
